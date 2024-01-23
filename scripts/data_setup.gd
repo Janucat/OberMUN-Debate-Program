cedb275291454acd159fd7768e3e8192c7db3bce
Janucat <jan.rovatti@gmail.com>
2024-01-23 18:47:50 +0100

    Initial commit

---
+++ scripts/data_setup.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+var json_data = preload("res://json/data.json")
+var nations_data = []
+var nations_id = []
+var nations_name = {}
+var nations_image = {}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	nations_data = json_data.data.get("nations")
+	
+	# Assigning ids to nations_id
+	for i in nations_data.size(): 
+		nations_id.append(nations_data[i].get("id"))
+		print(nations_id[i])
+	
+	# Assigning names to nations_name and nations_image
+	for i in nations_id.size():
+		nations_name.merge({
+			nations_id[i]: nations_data[i].get("name")
+		})
+		nations_image.merge({
+			nations_id[i]: nations_data[i].get("image")
+		})
+	
+	print(nations_image)
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
