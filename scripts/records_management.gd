c19830773cb7cb569511a814833383b4a0d25380
Janucat <jan.rovatti@gmail.com>
2024-03-07 22:52:06 +0100

    Update records_management.gd

--- scripts/records_management.gd
+++ scripts/records_management.gd
@@ -10,7 +10,10 @@ func _process(delta):
 	pass
 
 func makerecord(results):
-	var json_results = JSON.stringify(results)
+	var json_results = JSON.stringify(results, "\t")
+	var file = FileAccess.open("user://records/record.json", FileAccess.WRITE)
+	file.store_string(json_results)
+	#NOMERECORD avrà data e ora per evitare sovrapposizioni
 	
 #   var path = ""
 #	if OS.has_feature("editor"):
