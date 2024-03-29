9dfebbb38a037b1b677bbc0f36d369a2e1099fca
Janucat <jan.rovatti@gmail.com>
2024-03-29 16:08:22 +0100

    Update records_file_button.gd

--- records_file_button.gd
+++ records_file_button.gd
@@ -1,6 +1,9 @@
 extends LinkButton
 
 func _ready():
+	var dir = DirAccess.open("user://")
+	if !dir.dir_exists("user://records"):
+		var error = dir.make_dir("user://records")
 	uri = "/".join([OS.get_user_data_dir(), "records"])
 
 func _process(delta):
